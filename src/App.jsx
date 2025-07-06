958d45ce4c59a3f9f38b91584741b936c53faec1
Boni178 <felicityb08@gmail.com>
2025-07-06 18:00:03 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import "./index.css";
+/*import { FaChevronLeft, FaChevronRight,FaQuoteLeft,FaFacebook, FaTwitter,FaInstagram } from 'react-icons/fa';*/
+import Integrity from "./integrity.jsx";
+import Reviews from "./reviews.jsx";
+import Footer from "./footer.jsx";
+
+
+
+export default function App(){
+
+  
+return(
+<div className="smooth-scroll p-2 m-2"  id="container  "  >
+<Integrity></Integrity>
+<Reviews></Reviews>
+<Footer></Footer>
+     
+
+    
+    </div>
+     
+
+
+ 
+
+)
+
+}
