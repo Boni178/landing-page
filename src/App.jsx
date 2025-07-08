bd302c4237efc101d657f00e751a3ca44048cda4
Alphonce Maramba <marambaalphonce@gmail.com>
2025-07-08 18:05:56 +0300

    this is the addition of the 4 components that is header, about and specialty section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import "./index.css";
 import Integrity from "./integrity.jsx";
 import Reviews from "./reviews.jsx";
 import Footer from "./footer.jsx";
+import Header from './Header';
+import HeroSection from './HeroSection';
+import AboutUsSection from './AboutUsSection';
+import SpecialtySection from './Specialty';
 
 
 
@@ -11,6 +15,10 @@ export default function App(){
   
 return(
 <div className="smooth-scroll p-2 m-2"  id="container  "  >
+<Header></Header>
+<HeroSection></HeroSection>
+<AboutUsSection></AboutUsSection>
+<SpecialtySection></SpecialtySection>
 <Integrity></Integrity>
 <Reviews></Reviews>
 <Footer></Footer>
